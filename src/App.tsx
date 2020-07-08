ddb4f39143bc1033b6fc943b8790c78b15ba86aa
Syond <syond94@gmail.com>
2020-07-08 00:42:32 -0300

    Main component created. Header finished.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+import Layout from "./components/Layout";
+
 import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   return (
     <>
+      <Layout />
       <GlobalStyles />
-      <h1>Hello World</h1>
     </>
   );
 }
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Container, Wrapper } from './styles';
 
+import Main from '../Main';
+
 const Layout: React.FC = () => {
   return (
       <Container>
           <Wrapper>
               {/* <MenuBar /> */}
-              {/* <Main /> */}
+              <Main />
               {/* <Sidebar /> */}
           </Wrapper>
       </Container>
