00929c44e76be6580ff8f9ddf87e4dcee61727d2
Syond <syond94@gmail.com>
2020-07-11 21:42:27 -0300

    Used react-sticky-box to fix the Sidebar.

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,63 +1,54 @@
-import React from 'react';
+import React from "react";
+import StickyBox from "react-sticky-box";
 
-import { 
-    Container,
-    SearchWrapper,
-    SearchInput,
-    SearchIcon,
-    Body, 
-} from './styles';
+import {
+  Container,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  Body,
+} from "./styles";
 
-import List from '../List';
-import FollowSuggestion from '../FollowSuggestion';
-import News from '../News';
+import List from "../List";
+import FollowSuggestion from "../FollowSuggestion";
+import News from "../News";
 
 const SideBar: React.FC = () => {
   return (
-      <Container>
-          <SearchWrapper>
-              <SearchInput placeholder="Buscar no Twitter"/>
-              <SearchIcon />
-          </SearchWrapper>
+    <Container>
+      <SearchWrapper>
+        <SearchInput placeholder="Buscar no Twitter" />
+        <SearchIcon />
+      </SearchWrapper>
 
-          <Body>
-                <List 
-                    title="Talvez você curta"
-                    elements={[
-                        <FollowSuggestion 
-                            name="Ana Cláudia"
-                            nickname="@aninha"
-                        />,
-                        <FollowSuggestion 
-                            name="Victor Almeida"
-                            nickname="@victor21"
-                        />,
-                        <FollowSuggestion 
-                            name="Luis Carlos"
-                            nickname="@lc2013"
-                        />,
-                    ]}
-                />
-                <List 
-                    title="O que está acontecendo"
-                    elements={[
-                        <News 
-                            subject="Assuntos do momento no Brasil"
-                            description="Neymar cai de novo"
-                        />,
-                        <News 
-                            subject="Mundo"
-                            description="Fim da pandemia"
-                        />,
-                        <News 
-                            subject="#humor"
-                            description="Conheça as novas piadas de tiozão do churrasco"
-                        />,
-                    ]}
-                />
-          </Body>
-      </Container>
+      <StickyBox>
+        <Body>
+          <List
+            title="Talvez você curta"
+            elements={[
+              <FollowSuggestion name="Ana Cláudia" nickname="@aninha" />,
+              <FollowSuggestion name="Victor Almeida" nickname="@victor21" />,
+              <FollowSuggestion name="Luis Carlos" nickname="@lc2013" />,
+            ]}
+          />
+          <List
+            title="O que está acontecendo"
+            elements={[
+              <News
+                subject="Assuntos do momento no Brasil"
+                description="Neymar cai de novo"
+              />,
+              <News subject="Mundo" description="Fim da pandemia" />,
+              <News
+                subject="#humor"
+                description="Conheça as novas piadas de tiozão do churrasco"
+              />,
+            ]}
+          />
+        </Body>
+      </StickyBox>
+    </Container>
   );
-}
+};
 
 export default SideBar;
